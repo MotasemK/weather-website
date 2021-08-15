5aa7dbd741961fd1de136dc1d0a8cb93449935bd
MotasemK <mo3tasem66@gmail.com>
2021-08-15 19:00:02 +0300

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -1,101 +1,102 @@
-const path = require('path')
-const express = require('express')
-const hbs = require('hbs')
-const forecast = require('./utils/forecast')
-const geocode = require('./utils/geocode')
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+const forecast = require("./utils/forecast");
+const geocode = require("./utils/geocode");
 
-const app = express()
+const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
-const publicDirectory = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname,'../templates/views')
-const partialPath = path.join(__dirname, '../templates/partials')
+const publicDirectory = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialPath = path.join(__dirname, "../templates/partials");
 
 // Setup handlebars engine and views location
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialPath)
-
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialPath);
 
 // Setup static directory to serve
-app.use(express.static(publicDirectory))
+app.use(express.static(publicDirectory));
 
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Motasem Khalefa",
+  });
+});
 
-app.get('', (req, res) => {
-    res.render('index',{
-        title: 'Weather',
-        name: 'Motasem Khalefa'
-    })
-})
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About me",
+    name: "Motasem",
+  });
+});
 
-app.get('/about', (req, res) => {
-    res.render('about',{
-        title: 'About me',
-        name:'Motasem'
-    })
-})
+app.get("/help", (req, res) => {
+  res.render("help", {
+    message: "Contact us for more information",
+    title: "Help",
+    name: "Motasem khalefa",
+  });
+});
 
-app.get('/help', (req, res) => {
-    res.render('help',{
-        message: 'Contact us for more information',
-        title: 'Help',
-        name: 'Motasem khalefa' 
-    })
-})
-
-app.get('/weather', (req, res) => {
-    if (!req.query.address) {
-        return res.send({
-            error: 'You must provide an address'
-        })
-    }
-    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address",
+    });
+  }
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      }
+      forecast(latitude, longitude, (error, forecastData) => {
         if (error) {
-            return res.send({ error })
+          return res.send({ error });
         }
-        forecast(latitude, longitude, (error, forecastData) => {
-            if (error) {
-                return res.send({ error })
-            }
-            res.send({
-                forecast: forecastData,
-                location: location,
-                address: req.query.address
-            })
-        })
-    })
-})
-
-app.get('/products', (req, res) => {
-    if (!req.query.search) {
-        return res.send({
-            error: 'You must provide a search term'
-        })
+        res.send({
+          forecast: forecastData,
+          location: location,
+          address: req.query.address,
+        });
+      });
     }
+  );
+});
 
-    console.log(req.query)
-    res.send({
-        products: []
-    })
-})
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term",
+    });
+  }
 
-app.get('/help/*', (req, res) => {
-    res.render('404',{
-        title:'404',
-        name:'Motasem Khalefa',
-        message: 'Help article not found',
-    })
-})
+  console.log(req.query);
+  res.send({
+    products: [],
+  });
+});
 
-app.get('*', (req, res) => {
-    res.render('404',{
-        title:'404',
-        name:'Motasem Khalefa',
-        message: 'My 404 page',
-    })
-})
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Motasem Khalefa",
+    message: "Help article not found",
+  });
+});
 
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Motasem Khalefa",
+    message: "My 404 page",
+  });
+});
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
-})
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
